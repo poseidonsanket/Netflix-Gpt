6abd5f1a4e402702aa3aa8b6eab7604f47acd5bf
poseidonsanket <sanketdadali05@gmail.com>
2024-03-09 15:15:04 +0530

    Form Validation Done and Firebase Added

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -1,15 +1,17 @@
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAAFhZJUeZOpQw13JjXnmKH6_fEwsHNZ0w",
-  authDomain: "netflixgpt-5a7c6.firebaseapp.com",
-  projectId: "netflixgpt-5a7c6",
-  storageBucket: "netflixgpt-5a7c6.appspot.com",
-  messagingSenderId: "398348495903",
-  appId: "1:398348495903:web:af76b7da71e0c0106ab92a",
-  measurementId: "G-673C42F5N5",
+  apiKey: "AIzaSyAD2JrXyC70h3JioghrNUwJ8GcpFEC_YvE",
+  authDomain: "netflixgpt-c4114.firebaseapp.com",
+  projectId: "netflixgpt-c4114",
+  storageBucket: "netflixgpt-c4114.appspot.com",
+  messagingSenderId: "983297929048",
+  appId: "1:983297929048:web:cf03538f485837c7fd3f2d",
+  measurementId: "G-QLLS8DCY4B"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
