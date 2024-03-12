16ca9657c2079c511bc26c4c0de18a100241a52b
poseidonsanket <sanketdadali05@gmail.com>
2024-03-12 20:03:06 +0530

    Card Hover effect Added

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { IMG_CDN_URL } from "../utils/constants";
 
-const MovieCard = ({ poster_path }) => {
-  if (!poster_path) return;
+const MovieCard = ({ poster_path, title }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
-    <div className=" w-36 md:w-48 pr-2">
-      <img src={IMG_CDN_URL + poster_path} alt="moviecard" />
+    <div
+      className="w-36 md:w-48 pr-2 relative group overflow-hidden transform transition-transform ease-in-out duration-300 hover:scale-105"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <img
+        src={IMG_CDN_URL + poster_path}
+        alt="moviecard"
+        className="w-full h-auto"
+      />
+
+      {isHovered && (
+        <div className="absolute inset-0 bg-black bg-opacity-75 transition duration-500 ease-in-out opacity-0 group-hover:opacity-100">
+          <div className="absolute inset-0 flex items-center justify-center">
+            <div className="text-white text-center">
+              <h2 className="text-2xl font-semibold">{title}</h2>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import MovieCard from "./MovieCard";
 
 const MovieList = ({ title, movies }) => {
+  console.log(movies);
   return (
     <div className="px-6">
       <h1 className="text-lg md:text-4xl py-4 text-white">{title}</h1>
       <div className="flex overflow-x-auto no-scrollbar">
         <div className="flex">
           {movies?.map((movie) => (
-            <MovieCard key={movie.id} poster_path={movie.poster_path} />
+            <MovieCard
+              key={movie.id}
+              title={movie.title}
+              poster_path={movie.poster_path}
+            />
           ))}
         </div>
       </div>
