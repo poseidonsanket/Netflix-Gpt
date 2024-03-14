cb6e03a21b81154579e8c4865386b779fe755ccb
Sanket Dadali <112609618+poseidonsanket@users.noreply.github.com>
2024-03-14 06:46:12 +0000

    Open Ai key added

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 import { OPENAI_KEY } from "./constants";
 
 const openai = new OpenAI({
-  apiKey: OPENAI_KEY,
+  apiKey: OPENAI_KEY || import.meta.env.VITE_OPEN_AI_API_KEY,
   dangerouslyAllowBrowser: true,
 });
 
