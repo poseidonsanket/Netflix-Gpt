f4d000467c2967a836ed9b404bb42b3125d8f131
poseidonsanket <sanketdadali05@gmail.com>
2024-03-18 16:57:31 +0530

    Drop Down Added In Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../utils/Firebase";
 import { useNavigate } from "react-router-dom";
@@ -10,12 +10,15 @@ import { USER_AVATAR } from "../utils/constants";
 import { clearMovieResults, toggleGptSearchView } from "../utils/GptSlice";
 import { SUPPORTED_LANGUAGES } from "../utils/constants";
 import { changeLanguage } from "../utils/LanguageSlice";
+import { IoIosArrowDown } from "react-icons/io";
 
 const Header = () => {
   const navigate = useNavigate();
   const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
   const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
+  const [showDropdown, setShowDropdown] = useState(false);
+
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
@@ -25,7 +28,13 @@ const Header = () => {
         // An error happened.
         navigate("/error");
       });
+    handleToggleDropdown();
   };
+
+  const handleToggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -54,56 +63,57 @@ const Header = () => {
   const handleGptSearch = () => {
     dispatch(toggleGptSearchView());
     dispatch(clearMovieResults());
+    handleToggleDropdown();
   };
 
   const handleLanguageChange = (e) => {
     dispatch(changeLanguage(e.target.value));
   };
+
   return (
-    <div className="absolute px-12 py-8 bg-gradient-to-b from-black w-[100%] z-10 flex flex-col justify-between gap-5 md:flex-row md:gap-0">
+    <div className="absolute px-8 py-8 bg-gradient-to-b from-black w-[100%] z-10 flex justify-between gap-40 md:flex-row md:gap-0 md:px-12">
       <svg
         viewBox="0 0 111 30"
         version="1.1"
         aria-hidden="true"
         role="img"
         fill="rgb(229, 9, 20)"
-        className="w-[167px] h-[45px] mx-auto md:mx-0"
+        className="w-[167px] h-[45px] mx-0 md:mx-0"
       >
         <g>
           <path d="M105.06233,14.2806261 L110.999156,30 C109.249227,29.7497422 107.500234,29.4366857 105.718437,29.1554972 L102.374168,20.4686475 L98.9371075,28.4375293 C97.2499766,28.1563408 95.5928391,28.061674 93.9057081,27.8432843 L99.9372012,14.0931671 L94.4680851,-5.68434189e-14 L99.5313525,-5.68434189e-14 L102.593495,7.87421502 L105.874965,-5.68434189e-14 L110.999156,-5.68434189e-14 L105.06233,14.2806261 Z M90.4686475,-5.68434189e-14 L85.8749649,-5.68434189e-14 L85.8749649,27.2499766 C87.3746368,27.3437061 88.9371075,27.4055675 90.4686475,27.5930265 L90.4686475,-5.68434189e-14 Z M81.9055207,26.93692 C77.7186241,26.6557316 73.5307901,26.4064111 69.250164,26.3117443 L69.250164,-5.68434189e-14 L73.9366389,-5.68434189e-14 L73.9366389,21.8745899 C76.6248008,21.9373887 79.3120255,22.1557784 81.9055207,22.2804387 L81.9055207,26.93692 Z M64.2496954,10.6561065 L64.2496954,15.3435186 L57.8442216,15.3435186 L57.8442216,25.9996251 L53.2186709,25.9996251 L53.2186709,-5.68434189e-14 L66.3436123,-5.68434189e-14 L66.3436123,4.68741213 L57.8442216,4.68741213 L57.8442216,10.6561065 L64.2496954,10.6561065 Z M45.3435186,4.68741213 L45.3435186,26.2498828 C43.7810479,26.2498828 42.1876465,26.2498828 40.6561065,26.3117443 L40.6561065,4.68741213 L35.8121661,4.68741213 L35.8121661,-5.68434189e-14 L50.2183897,-5.68434189e-14 L50.2183897,4.68741213 L45.3435186,4.68741213 Z M30.749836,15.5928391 C28.687787,15.5928391 26.2498828,15.5928391 24.4999531,15.6875059 L24.4999531,22.6562939 C27.2499766,22.4678976 30,22.2495079 32.7809542,22.1557784 L32.7809542,26.6557316 L19.812541,27.6876933 L19.812541,-5.68434189e-14 L32.7809542,-5.68434189e-14 L32.7809542,4.68741213 L24.4999531,4.68741213 L24.4999531,10.9991564 C26.3126816,10.9991564 29.0936358,10.9054269 30.749836,10.9054269 L30.749836,15.5928391 Z M4.78114163,12.9684132 L4.78114163,29.3429562 C3.09401069,29.5313525 1.59340144,29.7497422 0,30 L0,-5.68434189e-14 L4.4690224,-5.68434189e-14 L10.562377,17.0315868 L10.562377,-5.68434189e-14 L15.2497891,-5.68434189e-14 L15.2497891,28.061674 C13.5935889,28.3437998 11.906458,28.4375293 10.1246602,28.6868498 L4.78114163,12.9684132 Z"></path>
         </g>
       </svg>
       {user && (
-        <div className="flex items-center gap-4 justify-center md:mx-0">
-          {showGptSearch && (
-            <select
-              className="p-2 bg-gray-900 text-white"
-              onChange={handleLanguageChange}
-            >
-              {SUPPORTED_LANGUAGES.map((lang) => (
-                <option key={lang.identifier} value={lang.identifier}>
-                  {lang.name}
-                </option>
-              ))}
-            </select>
-          )}
-          <button
-            className="px-4 py-2 text-white bg-red-700 rounded-lg"
-            onClick={handleGptSearch}
-          >
-            {showGptSearch ? "HomePage" : "GPT Search"}
-          </button>
-
-          <div className="flex items-center justify-center">
-            <img src={USER_AVATAR} />
+        <div className="flex items-center gap-4 justify-center md:mx-0 relative">
+          <div className="relative flex items-center gap-2 md:px-2">
+            <img
+              src={USER_AVATAR}
+              alt="User Avatar"
+              className="cursor-pointer"
+              onClick={handleToggleDropdown}
+            />
+            <IoIosArrowDown
+              className="none md:text-red-500 md:text-3xl md:cursor-pointer"
+              onClick={handleToggleDropdown}
+            />
+            {showDropdown && (
+              <div className="absolute top-full right-0 mt-2 bg-transparent rounded-lg shadow-lg w-36">
+                <button
+                  className="w-full py-2 px-4  my-2 text-left text-white bg-red-700 rounded-lg hover:bg-red-900 transition duration-200 flex items-center justify-center"
+                  onClick={handleGptSearch}
+                >
+                  {showGptSearch ? "Home Page" : "GPT Search"}
+                </button>
+                <button
+                  className="w-full py-2 px-4 text-left text-white bg-red-700 rounded-lg hover:bg-red-900 transition duration-200 flex items-center justify-center"
+                  onClick={handleSignOut}
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
           </div>
-
-          <button
-            className="font-bold text-white bg-red-700 rounded-lg p-3"
-            onClick={handleSignOut}
-          >
-            (Sign Out)
-          </button>
         </div>
       )}
     </div>
